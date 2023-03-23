dece4f8cabe0d8a47f4d9e29696361b10e2a00e9
ryxn <sheppardryan@rogers.com>
2023-03-23 18:42:13 -0400

    refactor: copy function from lambda ide

--- functions/ftp.py
+++ functions/ftp.py
@@ -2,19 +2,24 @@ import boto3
 import botocore
 
 
-def lambda_hander(event, context):
-    s3 = boto3.resource("s3")
-    file = None
+s3 = boto3.resource('s3')
 
-    bucket = event.get("bucket")
-    file_name = event.get("filename")
+
+def lambda_handler(event, context):
+    bucket = event["queryStringParameters"].get("bucket")
+    file_name = event["queryStringParameters"].get("filename")
 
     try:
         file = s3.meta.client.get_object(
             Bucket=bucket,
-            key=file_name)["Body"].read()
+            Key=file_name)["Body"].read()
     except botocore.exceptions.ClientError as error:
-        return {"error": f"There was an issue downloading your file... {error}"}
+        return {
+            "statusCode": 500,
+            "body": f"There was an issue downloading your file... {error}"
+        }
 
-    response = {"file": file}
-    return response
+    return {
+        'statusCode': 200,
+        'body': bytes.decode(file, "utf-8")
+    }
