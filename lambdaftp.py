cfe9bd4bf5481e51af5b805281e6a553f0e86f7e
ryxn <sheppardryan@rogers.com>
2023-03-23 18:40:50 -0400

    fix: dont write to file if request error

--- lambdaftp.py
+++ lambdaftp.py
@@ -2,15 +2,18 @@ import os
 import requests
 
 # ftp
-bucket_name = "test-bucket5807738"
-file_name = "s3commands.py"
+bucket_name = "1091670-harry"
+file_name = "GCloudComposer.py"
 
 query = requests.get(f"https://aysv85odkb.execute-api.us-east-2.amazonaws.com/\
 default/lambdaftp\
 ?bucket={bucket_name}\
 &filename={file_name}")
 
-os.makedirs("output", exist_ok=True)
-file_content = query.content
-with open(f"output/{file_name}", "wb") as file:
-    file.write(file_content)
+if query.status_code == 200:
+    os.makedirs("output", exist_ok=True)
+    file_content = query.content
+    with open(f"output/{file_name}", "wb") as file:
+        file.write(file_content)
+else:
+    print(f"An error ocurred: {bytes.decode(query.content, 'utf-8')}")
